23f327f6301c4f4a37418cd51703cb9db33645d3
Muhammad Ahsan <ahsan@vivaldi.net>
2022-06-01 01:09:29 +0200

    Update copyright year

--- CompanyC/package_statistics.py
+++ CompanyC/package_statistics.py
@@ -16,7 +16,7 @@ This script solves the problem described below:
 
 __author__ = 'Muhammad Ahsan'
 __version__ = '0.1'
-__copyright__ = 'Copyright (C) 2021 Muhammad Ahsan'
+__copyright__ = 'Copyright (C) 2022 Muhammad Ahsan'
 __license__ = 'CC BY-SA'
 
 import os.path
